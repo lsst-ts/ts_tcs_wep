159c18e5413f26bd8cd73d105639a1d332d388b6
Te-Wei Tsai <twtsaia@gmail.com>
2017-10-16 14:36:52 -0700

    Update IsrWrapper.py. Add EimgIsrWrapper.py

---
+++ wep/EimgIsrWrapper.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+
+import os, shutil
+import unittest
+
+from IsrWrapper import IsrWrapper
+
+class EimgIsrWrapper(IsrWrapper):
+
+	def doISR(self, visit, snap, raft, sensor, fakeDatasetType="eimage", outputDatasetType="postISRCCD"):
+		"""
+		
+		Do the image signature removal. This is just a simulation. The output is the eimage actually.
+		
+		Arguments:
+			visit {[int]} -- Visit time.
+			snap {int} -- Snap time (0 or 1) means first/ second exposure.
+			raft {[string]} -- Raft name.
+			sensor {[string]} -- Sensor name.
+
+		Keyword Arguments:
+			fakeDatasetType {[string]} -- Use this type of image supported by lsst camera mapper to simulate 
+								          the post-ISR image. (default: {"eimage"})
+			outputDatasetType {[string]} -- Output data type supported by lsst camera mapper. 
+									        (default: {"postISRCCD"})
+
+		Returns:
+			[exposure] -- Faked exposure image after ISR.
+		"""
+
+		# Exposure image after ISR
+		postIsrExposure = None
+
+		# Get the eimage data
+		dataId = dict(visit=visit, snap=snap, raft=raft, sensor=sensor)
+
+		# Pretend the eimage is the post ISR CCD
+		postIsrExposure = self.butler.get(fakeDatasetType, dataId=dataId)
+
+		# Put the data into the output path
+		self.butler.put(postIsrExposure, outputDatasetType, dataId=dataId)
+
+		return postIsrExposure
+
+class EimgIsrWrapperTest(unittest.TestCase):
+
+	"""
+    Test the function of EimageWfsIsrTask.
+    """
+
+	def setUp(self):
+
+		# Path of data folder
+		dataFolderPath = "/Users/Wolf/Documents/stash/ts_lsst_isr/test"
+		self.dataFolderPath = dataFolderPath
+
+	def testEimageWfsIsrTask(self):
+
+		# Instantiate the WFS ISR task
+		eIsrWrapper = EimgIsrWrapper(self.dataFolderPath, self.dataFolderPath)
+
+		# Setting for the ISR correction
+		obsId = 99999999
+		snap = 0
+		raft = "2,2"
+		sensor = "1,1"
+
+		# Do the ISR
+		postIsrExposure = eIsrWrapper.doISR(obsId, snap, raft, sensor, fakeDatasetType="eimage", 
+    									   outputDatasetType="postISRCCD")
+    	# Check the existence of file
+		filePath = os.path.join(self.dataFolderPath, "postISRCCD", "v99999999-fr", "R22", "S11.fits")
+		self.assertTrue(os.path.isfile(filePath))
+
+		# Remove the generated directory
+		shutil.rmtree(os.path.join(self.dataFolderPath, "postISRCCD"))
+
+if __name__ == "__main__":
+
+	# Do the unit test
+	unittest.main()
--- wep/IsrWrapper.py
+++ wep/IsrWrapper.py
@@ -1,24 +1,446 @@
+#!/usr/bin/env python
+
+import os
+
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.colors import LogNorm, SymLogNorm
+
+import lsst.daf.persistence as dafPersistence
+from lsst.ip.isr import IsrTask
+from lsst.ip.isr.assembleCcdTask import AssembleCcdTask
+
+import unittest
+
 class IsrWrapper(object):
 
-	def __init__(self):
-		pass
+	def __init__(self, inputs=None, outputs=None):
+		"""
+		
+		Initialize the WFS ISR task.
+		
+		Keyword Arguments:
+			inputs {[RepositoryArg or string]} -- Can be a single item or a list. Provides arguments 
+					to load an existing repository (or repositories). String is assumed to be a URI 
+					and is used as the cfgRoot (URI to the location of the cfg file). (Local file system 
+					URI does not have to start with 'file://' and in this way can be a relative path).
+					(default: {None})
+			outputs {[RepositoryArg or string]} -- Can be a single item or a list. Provides arguments to 
+					load one or more existing repositories or create new ones. String is assumed to be a 
+					URI and as used as the repository root.
+		"""
 
-	def generatePostIsrImg(self):
-		# Generate the post-ISR CCD image.
-		pass
+		self.pathData = inputs
+		self.outputPath = outputs
+		self.butler = dafPersistence.Butler(inputs=inputs, outputs=outputs)
+		self.config = None
 
-	def simulatePostIsrImg(self):
-		# Simulate the post-ISR CCD image by using eimage instead.
-		pass
+	def setConfig(self, doBias=True, doBrighterFatter=False, doDark=True, doDefect=True, doFlat=True, 
+				  doFringe=True, doLinearize=True, doWrite=False, overscanFitType="MEDIAN"):
+		"""
+		
+		Set up the configuration of image signature removal (ISR).
+		
+		Keyword Arguments:
+			doBias {bool} -- Apply bias frame correction? (default: {True})
+			doBrighterFatter {bool} -- Apply the brighter fatter correction? (default: {False})
+			doDark {bool} -- Apply dark frame correction? (default: {True})
+			doDefect {bool} -- Apply correction for CCD defects, e.g. hot pixels? (default: {True})
+			doFlat {bool} -- Apply flat field correction? (default: {True})
+			doFringe {bool} -- Apply fringe correction? (default: {True})
+			doLinearize {bool} -- Correct for nonlinearity of the detector's response? (default: {True})
+			doWrite {bool} -- Persist postISRCCD? (default: {False})
+			overscanFitType {string} -- The method for fitting the overscan bias level. (default: {"MEDIAN"})
+		"""
 
-	def configIsr(self):
-		# Do the setting of ISR.
-		pass
+		# Configuration of ISR
+		config = IsrTask.ConfigClass()
 
-	def doISR(self):
-		# Do the ISR.
-		pass
+		# Assemble amp-level exposures into a ccd-level exposure? (default: Ture in ConfigClass())
+		config.doAssembleCcd = False
 
-	def evalImgQual(self):
-		# Evaluate the image quality.
-		pass
+		# Assemble amp-level calibration exposures into ccd-level exposure? (default: False in ConfigClass())
+		config.doAssembleIsrExposures = False
+
+		# Apply bias frame correction? (default: Ture in ConfigClass())
+		config.doBias = doBias
+
+		# Apply the brighter fatter correction? (default: False in ConfigClass())
+		config.doBrighterFatter = doBrighterFatter
+
+		# Apply dark frame correction? (default: Ture in ConfigClass())
+		config.doDark = doDark
+
+		# Apply correction for CCD defects, e.g. hot pixels? (default: Ture in ConfigClass())
+		config.doDefect = doDefect
+
+		# Apply flat field correction? (default: Ture in ConfigClass())
+		config.doFlat = doFlat
+
+		# Apply fringe correction? (default: Ture in ConfigClass())
+		config.doFringe = doFringe
+
+		# Correct for nonlinearity of the detector's response? (default: Ture in ConfigClass())
+		config.doLinearize = doLinearize
+
+		# Persist postISRCCD? (default: Ture in ConfigClass())
+		config.doWrite = doWrite
+
+		# Set the type of overscan correction
+		config.overscanFitType = overscanFitType
+
+		# Set the configuration
+		self.config = config
+
+	def getButlerData(self, visit, snap, raft, sensor, channel=None, atype=None):
+		"""
+		
+		Get the butler data.
+		
+		Arguments:
+			visit {[int]} -- Visit time.
+			snap {int} -- Snap time (0 or 1) means first/ second exposure.
+			raft {[string]} -- Raft name.
+			sensor {[string]} -- Sensor name.
+
+		Keyword Arguments:
+			channel {[string]} -- Channel name (default: {"None"}).
+			atype {[string]} -- Type of arrangement: eimage, raw, bias, flat, dark, postISRCCD (default: {"None"}).
+		
+		Returns:
+			[butler] -- Butler data.
+		"""
+
+		# Define the data ID and get the raw amplifer image
+		if atype in ("eimage", "postISRCCD"):
+			dataId = dict(visit=int(visit), snap=snap, raft=raft, sensor=sensor, immediate=True)
+		else:
+			dataId = dict(visit=int(visit), snap=snap, raft=raft, sensor=sensor, channel=channel, immediate=True)
+		butlerData = self.butler.get(atype, dataId=dataId)
+
+		return butlerData
+
+	def doISR(self, visit, snap, raft, sensor, channel):
+		"""
+		
+		Do the image signature removal (ISR). (This is just the initial version for real ISR work. For the 
+		evaluation reason, the output is single channel. But it should be the ccd in the final.)
+		
+		Arguments:
+			visit {[int]} -- Visit time.
+			snap {int} -- Snap time (0 or 1) means first/ second exposure.
+			raft {[string]} -- Raft name.
+			sensor {[string]} -- Sensor name.
+			channel {[string]} -- Channel name.
+		
+		Returns:
+			[exposure] -- Exposure image after ISR.
+		"""
+
+		# Exposure image after ISR
+		postIsrExposure = None
+
+		# Define the data ID and get the raw amplifer image
+		# level in dataRef(): The level of dataId at which to reference
+		dataId = dict(visit=visit, snap=snap, raft=raft, sensor=sensor, channel=channel)
+		ampRef = self.butler.dataRef("raw", level=None, dataId=dataId)
+		ampExp = ampRef.get("raw")
+
+		# Do the isr task
+		lsstIsrTask = IsrTask(config=self.config)
+		isrData = lsstIsrTask.readIsrData(ampRef, ampExp)
+		postIsrExposure = lsstIsrTask.run(ampExp, **isrData.getDict()).exposure
+
+		return postIsrExposure
+
+	def overscanCorrectAllBias(self, visit, snap, raft, sensor, overscanFitType="MEDIAN"):
+		"""
+		
+		Do the overscan correction for bias frame of single sensor. This will replace the bias frame by the
+		overscan-corrected one.
+		
+		Arguments:
+			visit {[int]} -- Visit time.
+			snap {int} -- Snap time (0 or 1) means first/ second exposure.
+			raft {[string]} -- Raft name.
+			sensor {[string]} -- Sensor name.
+		
+		Keyword Arguments:
+			overscanFitType {string} -- The method for fitting the overscan bias level. 
+										(default: {"MEDIAN"})
+		"""
+
+		# Go through 16 amplifiers
+		overscanBiasList = {}
+		for ii in range(2):
+			for jj in range(8):
+
+				# Get the channel name
+				channel = str(ii) + "," + str(jj)
+
+				# Do the overscan correction for the single channel
+				self.__overscanCorrect(visit, snap, raft, sensor, channel, 
+									   overscanFitType=overscanFitType, doWrite=True)
+
+	def __overscanCorrect(self, visit, snap, raft, sensor, channel, overscanFitType="MEDIAN", doWrite=False):
+		"""
+		
+		Do the overscan correction for bias frame of single channel.
+		
+		Arguments:
+			visit {[int]} -- Visit time.
+			snap {int} -- Snap time (0 or 1) means first/ second exposure.
+			raft {[string]} -- Raft name.
+			sensor {[string]} -- Sensor name.
+			channel {[string]} -- Channel name.
+		
+		Keyword Arguments:
+			overscanFitType {string} -- The method for fitting the overscan bias level. (default: {"MEDIAN"})
+			doWrite {bool} -- Overwrite the file or not. (default: {False})
+		
+		Returns:
+			[Exposure] -- Overscan corrected bias frame.
+		"""
+
+		# Degine the data ID and get the bias frame
+		dataId = dict(visit=visit, snap=snap, raft=raft, sensor=sensor, channel=channel)
+		biasFrame = self.butler.get("bias", dataId=dataId)
+
+		# Define the configuration
+		config = self.setConfig(doBias=False, doBrighterFatter=False, doDark=False, doDefect=False, 
+								doFlat=False, doFringe=False, doLinearize=False, doWrite=False, 
+								overscanFitType=overscanFitType)
+
+		# Correct the overscan
+		# Do the isr task
+		lsstIsrTask = IsrTask(config=config)
+		overscanBias = lsstIsrTask.run(biasFrame).exposure
+
+		# Overwrite into file
+		if (doWrite):
+
+			# Put the overscan corrected bias frame
+			self.butler.put(overscanBias, "bias", dataId=dataId)
+
+			# Get the file Name
+			raftFolder = "R"+"".join(raft.split(","))
+			sensorFolder = "S"+"".join(sensor.split(","))
+			fileName = "_".join(["imsim", "0", raftFolder, sensorFolder, "C"+"".join(channel.split(","))]) + ".fits.gz"
+
+			# Get the file path
+			filePath = os.path.join(self.outputPath, "bias", "v0", raftFolder, sensorFolder, fileName)
+
+			# Check the existence of file
+			if (os.path.exists(filePath)):
+				os.remove(filePath)
+
+		return overscanBias
+
+	def assembleAmpImg(self, visit, snap, raft, sensor, atype=None, doISR=False, doWrite=False, 
+						outputDatasetType="postISRCCD"):
+		"""
+		
+		Assemble amplifier images into single CCD image.
+		
+		Arguments:
+			visit {[int]} -- Number of visit.
+			snap {[int]} -- Snap of exposure image. There are two times of exposure in single visit.
+							The snap value can be 0 or 1.
+			raft {[string]} -- Name of raft.
+			sensor {[string]} -- Name of sensor.
+
+		Keyword Arguments:
+			atype {[string]} -- Type of data: raw, bias, flat, dark (default: {"None"}).
+			doISR {[bool]} -- Do ISR or not? (True or False) (default: {False}).
+			doWrite {bool} -- Persist assembled ccd image? (default: {False}) 
+			outputDatasetType {[string]} -- Output data type supported by lsst camera mapper. 
+						        			(default: {"postISRCCD"})
+		
+		Returns:
+			[butler] -- Data butler of exposure image.
+		"""
+
+		# Assemble the amplifier images to get the single CCD image
+		# Contruct the dictionary for amplifier images on certain CCD
+		ampExp = {}
+		# Go through 16 amplifiers
+		for ii in range(2):
+			for jj in range(8):
+				singleChannel = str(ii) + "," + str(jj)
+
+				if (doISR is True and atype == "raw"):
+					ampExp[singleChannel] = self.doISR(visit, snap, raft, sensor, singleChannel)
+				elif (doISR is False and atype is not None):
+					ampExp[singleChannel] = self.getButlerData(visit, snap, raft, sensor, 
+															   channel=singleChannel, atype=atype)
+				else:
+					return
+
+		# Instantiate the AssembleCcdTask
+		AssembleCCD = AssembleCcdTask()
+		outExposure = AssembleCCD.assembleCcd(ampExp)
+
+		# Persist the assembled ccd image
+		if (doWrite):
+			
+			# Get the data ID
+			dataId = dict(visit=visit, snap=snap, raft=raft, sensor=sensor)
+
+			# Put the data into the output path
+			self.butler.put(outExposure, outputDatasetType, dataId=dataId)
+
+		return outExposure
+
+def poltExposureImage(exposure, name="", scale="log", cmap="gray", vmin=None, vmax=None):
+	"""
+	
+	Plot the exposure image.
+	
+	Arguments:
+		exposure {[exposure]} -- Data butler of exposure image.
+	
+	Keyword Arguments:
+		name {string} -- Image title name (default: {""}).
+		scale {string} -- Scale of image map (log or linear) (default: {"log"}).
+		cmap {string} -- Color map definition (default: {"gray"}).
+		vmin {[float]} -- Mininum value to show. This normalizes the luminance data (default: {None}).
+		vmax {[float]} -- Maximum value to show. This normalizes the luminance data (default: {None}).		
+	"""
+	# Get the image data
+	img = getImageData(exposure)
+
+	# Change the scale if needed
+	if scale not in ("linear", "log"):
+		print("No %s scale to choose. Only 'linear' and 'log' scales are allowed." % scale)
+		return
+
+	# Decide the norm in imshow for the ploting
+	if (scale == "linear"):
+		plotNorm = None
+	elif (scale == "log"):
+		if (img.min()) < 0:
+			plotNorm = SymLogNorm(linthresh=0.03)
+		else:
+			plotNorm = LogNorm()
+	
+	# Plot the image
+	plt.figure()
+	plt.imshow(img, cmap=cmap, origin="lower", norm=plotNorm, vmin=vmin, vmax=vmax)
+	plt.colorbar()
+	plt.title(name)
+	plt.show()
+
+def plotHist(exposure, name="", numOfBin=1000, log=False):
+	"""
+	
+	Plot the histogram.
+	
+	Arguments:
+		exposure {[exposure]} -- Data butler of exposure image.
+	
+	Keyword Arguments:
+		name {string} -- Image title name (default: {""}).
+		numOfBin {int} -- Number of bins (default: {1000}).
+		log {bool} -- The histogram axis will be set to a log scale if log=True 
+					  (default: {False}).
+	"""
+
+	# Get the image data
+	img = getImageData(exposure)
+
+	# Plot the histogram
+	plt.figure()
+	plt.hist(img.flatten(), bins=int(numOfBin), log=log)
+	plt.title(name)
+	plt.show()
+
+def getImageData(image):
+	"""
+	
+	Get the image data in numpy array.
+	
+	Arguments:
+		image {[obj]} -- Image data. It can be numpy array or Exposure image.
+	
+	Returns:
+		[float] -- Image data in numpy array.
+	"""
+
+	# Get the numpy array data based on the input object type
+	if isinstance(image, np.ndarray):
+		data = image
+	elif hasattr(image, "getMaskedImage"):
+		data = image.getMaskedImage().getImage().getArray() 
+	elif hasattr(image, "getImage"):
+		data = image.getImage().getArray() 
+	else:
+	    data = image.getArray()
+
+	# Return the data in numpy array
+	return data
+
+class IsrWrapperTest(unittest.TestCase):
+	
+	"""	
+	Test the function of WfsIsrTask.
+	"""
+
+	def setUp(self):
+
+		# Path of data folder
+		dataFolderPath = "/Users/Wolf/Documents/stash/ts_lsst_isr/test"
+		self.dataFolderPath = dataFolderPath
+
+	def testWfsIsrTask(self):
+
+		# Setting for the ISR correction
+		obsId = 99999999
+		snap = 0
+		raft = "2,2"
+		sensor = "1,1"
+		channel = "1,4"
+
+		# Instantiate the WFS ISR task
+		isrWrapper = IsrWrapper(inputs=self.dataFolderPath, outputs=self.dataFolderPath)
+
+		# Test the function of data butler
+		visit = isrWrapper.butler.queryMetadata("raw", ["visit"], dataId={"snap":0})
+		self.assertEqual(len(visit), 6)
+
+		# Set ISR configuration
+		isrWrapper.setConfig(doBias=True, doBrighterFatter=False, doDark=True, 
+							 doDefect=True, doFlat=True, doFringe=True, 
+							 doLinearize=True, doWrite=False, overscanFitType="MEDIAN")
+		
+		# Test the setting of configuration
+		self.assertEqual(isrWrapper.config.doWrite, False)
+
+		# Test to get the butler data
+		butlerDataRaw = isrWrapper.getButlerData(obsId, snap, raft, sensor, channel=channel, atype="raw")
+		self.assertEqual(butlerDataRaw.getMetadata().get("GAIN"), 1.83546)
+
+		# Test to assemble the amplifier images to single CCD
+		ccdExp = isrWrapper.assembleAmpImg(obsId, snap, raft, sensor, atype="raw", doISR=False, doWrite=False)
+		self.assertEqual(ccdExp.getDimensions()[0], 4072)
+		self.assertEqual(ccdExp.getDimensions()[1], 4000)
+
+		# Show the raw image
+		# poltExposureImage(ccdExp, name="Raw image")
+
+		# Test to do the ISR
+		# Before the ISR. Some values are high for the cosmic ray.
+		maxRaw = np.max(butlerDataRaw.getMaskedImage().getImage().getArray())
+		self.assertGreater(maxRaw, 1000)
+
+		postIsrExposure = isrWrapper.doISR(obsId, snap, raft, sensor, channel)
+		self.assertEqual(postIsrExposure.getDimensions()[0], 513)
+		self.assertEqual(postIsrExposure.getDimensions()[1], 2001)
+
+		# The cosmic ray shall be corrected.
+		maxIsr = np.max(postIsrExposure.getMaskedImage().getImage().getArray())
+		self.assertLess(maxIsr, 1)
+
+if __name__ == "__main__":
+
+	# Do the unit test
+	unittest.main()
