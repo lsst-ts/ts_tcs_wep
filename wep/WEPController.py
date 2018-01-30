f9d69a51fb64fc2416f6964c43efb642e26e2361
Te-Wei Tsai <TTsai@lsst.org>
2018-01-30 12:26:51 -0700

    Add functions to get defocal images

--- wep/WEPController.py
+++ wep/WEPController.py
@@ -1,10 +1,13 @@
 import os
+import numpy as np
 
 from wep.WFDataCollector import WFDataCollector
 from wep.EimgIsrWrapper import EimgIsrWrapper
 from wep.SourceSelector import SourceSelector
 from wep.SourceProcessor import SourceProcessor
 from wep.WFEstimator import WFEstimator
+
+from wep.IsrWrapper import getImageData
 
 class WEPController(object):
 
@@ -55,13 +58,14 @@ class WEPController(object):
         """
 
         self.dataCollector.config(pathOfRawData=pathOfRawData, destinationPath=destinationPath, 
-                            dbAdress=dbAdress, butlerInputs=butlerInputs, butlerOutputs=butlerOutputs)
+                        dbAdress=dbAdress, butlerInputs=butlerInputs, butlerOutputs=butlerOutputs)
 
-    def importPhoSimDataToButler(self, dataDir, obsId=None, aFilter=None, atype=None, overwrite=False):
+    def importPhoSimDataToButler(self, dataDir, obsId=None, aFilter=None, atype=None, 
+                                 overwrite=False):
         """
         
-        Import the PhoSim simulated data to match with the data butler to use. This means the registry.sqlite3 
-        repo will be inserted with the meta data if necessary.
+        Import the PhoSim simulated data to match with the data butler to use. This means the 
+        registry.sqlite3 repo will be inserted with the meta data if necessary.
         
         Arguments:
             dataDir {[str]} -- PhoSim FITS data directory.
@@ -78,6 +82,88 @@ class WEPController(object):
 
         self.dataCollector.importPhoSimDataToButler(dataDir=dataDir, obsId=obsId, aFilter=aFilter, 
                                                                     atype=atype, overwrite=overwrite)
+
+    def getButlerData(self, datasetType, dataId=None, immediate=True):
+        """
+        
+        Retrieves a dataset given an input collection data id.
+        
+        Arguments:
+            datasetType {[str]} -- The type of dataset to retrieve.
+        
+        Keyword Arguments:
+            dataId {[dict]} -- The data id. (default: {None})
+            immediate {bool} -- If False use a proxy for delayed loading. (default: {True})
+        
+        Returns:
+            [ExposureU] -- Exposure data.
+        """
+
+        return self.dataCollector.butler.get(datasetType=datasetType, dataId=dataId, 
+                                             immediate=immediate)
+
+    def getDefocalImg(self, snap, raft, sensor, intraObsId, extraObsId, datasetType="eimage", 
+                            immediate=True):
+        """
+        
+        Get the defocal images. The defocal images are based on the visits with different piston 
+        position.
+        
+        Arguments:
+            snap {[int]} -- Snap number.
+            raft {[str]} -- Raft name (e.g. "2,2").
+            sensor {[str]} -- Sensor name (e.g. "1,1").
+            intraObsId {[int]} -- Observation ID of intra-focal exposure.
+            extraObsId {[int]} -- Observation ID of extra-focal exposure.
+        
+        Keyword Arguments:
+            datasetType {str} -- The type of dataset to retrieve. (default: {"eimage"})
+            immediate {bool} -- If False use a proxy for delayed loading. (default: {True})
+        
+        Returns:
+            [ndarray] -- Intra-focal image.
+            [ndarray] -- Extra-focal image.
+        """
+
+        # Get the images
+        intraImg = self.__getImgData(intraObsId, snap, raft, sensor, datasetType=datasetType, 
+                                     immediate=immediate)
+        extraImg = self.__getImgData(extraObsId, snap, raft, sensor, datasetType=datasetType, 
+                                     immediate=immediate)
+
+        return intraImg, extraImg
+
+    def __getImgData(self, obsId, snap, raft, sensor, datasetType, immediate):
+        """
+        
+        Get the image data from exposure by data butler.
+        
+        Arguments:
+            obsId {[int]} -- Observation ID.
+            snap {[int]} -- Snap number.
+            raft {[str]} -- Raft name (e.g. "2,2").
+            sensor {[str]} -- Sensor name (e.g. "1,1").
+            datasetType {[str]} -- The type of dataset to retrieve.
+            immediate {[bool]} -- If False use a proxy for delayed loading.
+        
+        Returns:
+            [ndarray] -- Image data.
+        """
+        
+        # Enforce the number type
+        snap = int(snap)
+        obsId = int(obsId)
+
+        # Set the data Id
+        dataId = dict(visit=obsId, snap=snap, raft=raft, sensor=sensor)
+
+        # Get the exposure 
+        exposure = self.getButlerData(datasetType, dataId=dataId, immediate=immediate)
+
+        # Get the image
+        img = getImageData(exposure)
+
+        return img
 
 if __name__ == "__main__":
     
@@ -96,16 +182,21 @@ if __name__ == "__main__":
                                     butlerOutputs=butlerOutputs)
 
     # Import the PhoSim simulated image
-    obsId = 9006000
+    extraObsId = 9007000
+    intraObsId = 9007001
+    obsIdList = [extraObsId, intraObsId]
+    aFilter = "g"
+
+    dataDirList = ["realComCam/output/Extra", "realComCam/output/Intra"]
+    atype = "raw"
+    for ii in range(2):
+        wepCntlr.importPhoSimDataToButler(dataDirList[ii], obsId=obsIdList[ii], aFilter=aFilter, atype=atype, overwrite=False)
+
+    # Get the image data
     snap = 0
     raft = "2,2"
     sensor = "1,1"
+    intraImg, extraImg = wepCntlr.getDefocalImg(snap, raft, sensor, intraObsId, extraObsId, datasetType="eimage")
 
-    aFilter = "g"
+    # Get the image data
 
-    dataDir = "realWfs/output"
-    atype = "raw"
-    wepCntlr.importPhoSimDataToButler(dataDir, obsId=obsId, aFilter=aFilter, atype=atype, overwrite=False)
-
-    # Let the butler to hold the data
-    
