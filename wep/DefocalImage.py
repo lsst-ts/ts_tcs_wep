e1213a6cbb1362af378333aaf99eee893d27ec9c
Te-Wei Tsai <TTsai@lsst.org>
2018-02-02 09:45:52 -0700

    Add the DefocalImgae.py

---
+++ wep/DefocalImage.py
@@ -0,0 +1,10 @@
+class DefocalImage(object):
+
+    def __init__(self, sensorName, intraImg, extraImg):
+        
+        self.sensorName = sensorName
+        self.intraImg = intraImg
+        self.extraImg = extraImg
+
+if __name__ == "__main__":
+    pass
