b86d7cf61a0b5a9bfb26d8521258b7c9a5718f83
Te-Wei Tsai <TTsai@lsst.org>
2018-02-05 13:52:32 -0700

    Add the DonutImage class

--- wep/DefocalImage.py
+++ wep/DefocalImage.py
@@ -29,5 +29,27 @@ class DefocalImage(object):
         if (extraImg is not None):
             self.extraImg = extraImg
 
+class DonutImage(DefocalImage):
+
+    def __init__(self, starId, pixelX, pixelY, intraImg=None, extraImg=None):
+        """
+        
+        Initialize the DonutImage class.
+        
+        Arguments:
+            starId {[int]} -- star ID.
+            pixelX {[float]} -- Pixel x.
+            pixelY {[float]} -- Pixel y.
+        
+        Keyword Arguments:
+            intraImg {[ndarray]} -- Intra-defocal image. (default: {None})
+            extraImg {[ndarray]} -- Extra-defocal image. (default: {None})
+        """
+
+        self.starId = int(starId)
+        self.pixelX = pixelX
+        self.pixelY = pixelY
+        super(DonutImage, self).__init__(intraImg=intraImg, extraImg=extraImg)
+
 if __name__ == "__main__":
     pass
