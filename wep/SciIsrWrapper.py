686695d564542b6506ecc12f0aff7da48f23a1e1
Te-Wei Tsai <TTsai@lsst.org>
2018-03-14 12:09:54 -0700

    Add the plot functions

--- wep/SciIsrWrapper.py
+++ wep/SciIsrWrapper.py
@@ -1,6 +1,12 @@
 import os, unittest
 import numpy as np
 
+import matplotlib
+# Must be before importing matplotlib.pyplot or pylab!
+matplotlib.use("Agg")
+import matplotlib.pyplot as plt
+from matplotlib.colors import LogNorm, SymLogNorm
+
 from lsst.daf.persistence import Butler
 
 from wep.SciWFDataCollector import runProgram
@@ -132,6 +138,69 @@ def getImageData(image):
 
     # Return the data in numpy array
     return data
+
+def poltExposureImage(exposure, name="", scale="log", cmap="gray", vmin=None, vmax=None):
+    """
+    
+    Plot the exposure image.
+    
+    Arguments:
+        exposure {[exposure]} -- Data butler of exposure image.
+    
+    Keyword Arguments:
+        name {string} -- Image title name (default: {""}).
+        scale {string} -- Scale of image map (log or linear) (default: {"log"}).
+        cmap {string} -- Color map definition (default: {"gray"}).
+        vmin {[float]} -- Mininum value to show. This normalizes the luminance data (default: {None}).
+        vmax {[float]} -- Maximum value to show. This normalizes the luminance data (default: {None}).      
+    """
+    # Get the image data
+    img = getImageData(exposure)
+
+    # Change the scale if needed
+    if scale not in ("linear", "log"):
+        print("No %s scale to choose. Only 'linear' and 'log' scales are allowed." % scale)
+        return
+
+    # Decide the norm in imshow for the ploting
+    if (scale == "linear"):
+        plotNorm = None
+    elif (scale == "log"):
+        if (img.min()) < 0:
+            plotNorm = SymLogNorm(linthresh=0.03)
+        else:
+            plotNorm = LogNorm()
+    
+    # Plot the image
+    plt.figure()
+    plt.imshow(img, cmap=cmap, origin="lower", norm=plotNorm, vmin=vmin, vmax=vmax)
+    plt.colorbar()
+    plt.title(name)
+    plt.show()
+
+def plotHist(exposure, name="", numOfBin=1000, log=False):
+    """
+    
+    Plot the histogram.
+    
+    Arguments:
+        exposure {[exposure]} -- Data butler of exposure image.
+    
+    Keyword Arguments:
+        name {string} -- Image title name (default: {""}).
+        numOfBin {int} -- Number of bins (default: {1000}).
+        log {bool} -- The histogram axis will be set to a log scale if log=True 
+                      (default: {False}).
+    """
+
+    # Get the image data
+    img = getImageData(exposure)
+
+    # Plot the histogram
+    plt.figure()
+    plt.hist(img.flatten(), bins=int(numOfBin), log=log)
+    plt.title(name)
+    plt.show()
 
 class SciIsrWrapperTest(unittest.TestCase):
 
