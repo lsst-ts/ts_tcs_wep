ecce8b610ed08409bda5bd08bb8592063a574dac
Te-Wei Tsai <TTsai@lsst.org>
2018-03-23 09:12:16 -0700

    Add the comment to middle ware related functions

--- wep/WEPController.py
+++ wep/WEPController.py
@@ -64,6 +64,16 @@ class WEPController(object):
         self.__setVar(wfsEsti, "wfsEsti")
 
     def setMiddleWare(self, topicList, moduleName="tcsWEP"):
+        """
+        
+        Set the middle ware. 
+        
+        Arguments:
+            topicList {[list]} -- List of topic.
+        
+        Keyword Arguments:
+            moduleName {[str]} -- Module name. (default: {"tcsWEP"})
+        """
 
         if moduleName not in list(self.topicList.keys()):
             self.topicList[moduleName] = np.array(topicList)
@@ -71,23 +81,68 @@ class WEPController(object):
             for ii in range(len(topicList)):
                 middleWareList.append(Middleware(moduleName))
             self.middleWare[moduleName] = middleWareList
+        else:
+            print("The module: '%s' is in the list ready." % moduleName)
 
-    def shutDownMiddleWare(self):
+    def shutDownSal(self):
+        """
+        
+        Shutdown the SAL.
+        """
 
         for moduleName, middleWareList in self.middleWare.items():
-            print("Turn off SAL module: %s" % moduleName)
+            print("Shutdown SAL module: %s" % moduleName)
             for middleWare in middleWareList:
                 middleWare.shutDownSal()
 
     def issueEvent(self, topic, newData, moduleName="tcsWEP"):
+        """
+        
+        Issue the event.
+        
+        Arguments:
+            topic {[str]} -- Topic name.
+            newData {[dict]} -- New input data to issue.
+        
+        Keyword Arguments:
+            moduleName {[str]} -- Module name. (default: {"tcsWEP"})
+        """
 
-        idx = np.where(self.topicList[moduleName] == topic)[0][0]        
+        idx = self.__getIdxOfTopic(topic, moduleName)
         self.middleWare[moduleName][idx].issueEvent(topic, newData)
 
     def issueTelemetry(self, topic, newData, moduleName="tcsWEP"):
+        """
+        
+        Issue the telemetry.
+        
+        Arguments:
+            topic {[str]} -- Topic name.
+            newData {[dict]} -- New input data to issue.
+        
+        Keyword Arguments:
+            moduleName {[str]} -- Module name. (default: {"tcsWEP"})
+        """
 
-        idx = np.where(self.topicList[moduleName] == topic)[0][0]  
+        idx = self.__getIdxOfTopic(topic, moduleName)
         self.middleWare[moduleName][idx].issueTelemetry(topic, newData)
+
+    def __getIdxOfTopic(self, topic, moduleName):
+        """
+        
+        Get the index of specific topic in specific module. 
+        
+        Arguments:
+            topic {[str]} -- Topic name.
+            moduleName {[str]} -- Module name.
+        
+        Returns:
+            [int] -- Index of topic.
+        """
+
+        idx = np.where(self.topicList[moduleName] == topic)[0][0]
+
+        return idx
 
     def getWfsList(self):
         """
@@ -297,7 +352,6 @@ class WEPController(object):
                             fakeDatasetType=fakeDatasetType, outputDatasetType=outputDatasetType)
         else:
             raise RuntimeError("Sensor name: '%s' is not allowed." % sensorName)
-
 
     def __getSensorInfo(self, sensorName):
         """
@@ -761,7 +815,7 @@ class WEPController(object):
         """
         
         # Calculate the wavefront error
-        for sensorName, donutImgList in donutMap.items():
+        for donutImgList in donutMap.values():
             for donutImg in donutImgList:
                 donutImg = self.calcSglWfErr(donutImg)
 
@@ -822,10 +876,6 @@ class WEPController(object):
         avgErr = avgErr/np.sum(weightingRatio)
 
         return avgErr
-
-    def publishWfsErr(self, sensorName, wfsErr):
-
-        pass
 
 def calcWeiRatio(donutImgList):
     """
@@ -1203,5 +1253,5 @@ if __name__ == "__main__":
         time.sleep(1)
 
     time.sleep(20)
-    wepCntlr.shutDownMiddleWare()
+    wepCntlr.shutDownSal()
 
