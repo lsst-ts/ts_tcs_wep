3951050c2a76a19a1f065bebff9e101dad27d0a6
Te-Wei Tsai <TTsai@lsst.org>
2018-04-02 15:42:34 -0700

    Add the unit test

--- wep/DefocalImage.py
+++ wep/DefocalImage.py
@@ -1,3 +1,5 @@
+import unittest
+
 class DefocalImage(object):
 
     def __init__(self, intraImg=None, extraImg=None):
@@ -70,5 +72,48 @@ class DonutImage(DefocalImage):
 
         self.zer4UpNm = zer4UpNm
 
+class DefocalImageTest(unittest.TestCase):
+
+    """ 
+    Test the function of DefocalImage.
+    """
+
+    def setUp(self):
+        
+        self.defocalImg = DefocalImage()
+
+    def testFunction(self):
+        
+        intraImg = 1
+        extraImg = 2
+        self.defocalImg.setImg(intraImg=intraImg, extraImg=extraImg)
+        self.assertEqual(self.defocalImg.intraImg, intraImg)
+        self.assertEqual(self.defocalImg.extraImg, extraImg)
+
+class DonutImageTest(unittest.TestCase):
+
+    """ 
+    Test the function of DonutImage.
+    """
+
+    def setUp(self):
+        
+        starId = 0 
+        pixelX = 1 
+        pixelY = 1 
+        fieldX = 2 
+        fieldY = 2
+        self.donutImg = DonutImage(starId, pixelX, pixelY, fieldX, fieldY)
+
+    def testFunction(self):
+        
+        self.assertEqual(self.donutImg.pixelX, self.donutImg.pixelY)
+
+        zer4UpNm = 10
+        self.donutImg.setWfErr(zer4UpNm)
+        self.assertEqual(self.donutImg.zer4UpNm, zer4UpNm)
+
 if __name__ == "__main__":
-    pass
+    
+    # Do the unit test
+    unittest.main()
