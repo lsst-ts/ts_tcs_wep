0fded343bb08adea6980262a32d2cf32df5c7c72
Te-Wei Tsai <TTsai@lsst.org>
2018-12-17 11:38:14 -0700

    Fix the module path.

--- python/lsst/ts/wep/Utility.py
+++ python/lsst/ts/wep/Utility.py
@@ -1,26 +1,30 @@
 import os
-from pathlib import Path
 import lsst.ts.wep
 
-def getModulePath(module=lsst.ts.wep, startIdx=3, endIdx=-4):
-    """
-    
-    Get the directory of WEP module.
 
-    Keyword Arguments:
-    	[module] -- Module.
-    	[int] -- Start index. (Default: {3})
-    	[int] -- End index. (Default: {-4})
-    
-    Returns:
-        [str] -- Directory of module based on the start and end indexes.
+def getModulePath(module=lsst.ts.wep, startIdx=1, endIdx=-4):
+    """Get the path of module.
+    Parameters
+    ----------
+    module : str, optional
+        Module name. (the default is lsst.ts.ofc.)
+    startIdx : int, optional
+        Start index. (the default is 1.)
+    endIdx : int, optional
+        End index. (the default is -4.)
+    Returns
+    -------
+    str
+        Directory path of module based on the start and end indexes.
     """
 
     # Get the path of module
-    modulePathList = os.path.dirname(module.__file__).split(os.sep)[startIdx:endIdx]
-    modulePath = os.path.join(str(Path.home()), *modulePathList)
-    
+    modulePathList = os.path.dirname(module.__file__).split(
+                                os.sep)[int(startIdx):int(endIdx)]
+    modulePath = os.path.join(os.sep, *modulePathList)
+
     return modulePath
+
 
 if __name__ == "__main__":
     pass
