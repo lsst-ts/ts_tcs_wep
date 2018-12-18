5832c4c683fd421f4833525c78add1a02d1ed363
Te-Wei Tsai <TTsai@lsst.org>
2018-12-18 13:25:23 -0700

    Add the test script of LocalDatabase.

--- python/lsst/ts/wep/isr/LocalDatabase.py
+++ python/lsst/ts/wep/isr/LocalDatabase.py
@@ -1,7 +1,6 @@
-import os, sqlite3, unittest
+import os, sqlite3
 import numpy as np
 
-from lsst.ts.wep.Utility import getModulePath
 
 class LocalDatabase(object):
 
@@ -211,65 +210,6 @@ class LocalDatabase(object):
 
         return self.cursor.fetchall()
 
-class LocalDatabaseTest(unittest.TestCase):
-    
-    """
-    Test the function of LocalDatabase.
-    """
-
-    def setUp(self):
-
-        # Get the path of module
-        modulePath = getModulePath()
-
-        # Local database setting
-        dbAdress = os.path.join(modulePath, "test", "registry.sqlite3")
-        self.dbAdress = dbAdress
-
-    def testLocalDatabase(self):
-
-        # Connect the local database
-        localDatabase = LocalDatabase()
-        localDatabase.connect(self.dbAdress)
-
-        # Parameters from Phosim
-        obsId = 99999998
-        snap = 0
-        raft = "2,2"
-        sensor = "1,1"
-        aFilter = "r"
-
-        # Pretend to have a visit time (tai: International Atomic Time)
-        taiObs = "1994-07-19T06:49:51.543999744"
-
-        # Pretend to have the skyTile
-        skyTile = 30001
-
-        # Exposure time is 15 sec in single snap
-        expTime = 15.0
-
-        # Test to query a certain visit
-        item = localDatabase.query("raw", visit=obsId, snap=snap, raft=raft, sensor=sensor, aFilter=aFilter)
-        self.assertEqual(item, [])
-
-        # Test to insert the data
-        localDatabase.insertData(obsId, snap, raft, sensor, aFilter, taiObs, skyTile, expTime)
-
-        # Query the inserted data
-        item = localDatabase.query("raw", visit=obsId, snap=snap, raft=raft, sensor=sensor, aFilter=aFilter)
-        self.assertNotEqual(item, [])
-
-        # Test to delete the data
-        localDatabase.deleteData(obsId, snap, raft, sensor, aFilter)
-
-        # Query the inserted data
-        item = localDatabase.query("raw", visit=obsId, snap=snap, raft=raft, sensor=sensor, aFilter=aFilter)
-        self.assertEqual(item, [])
-
-        # Disconnect from the local database
-        localDatabase.disconnect()
 
 if __name__ == "__main__":
-
-    # Do the unit test
-    unittest.main()
+    pass
