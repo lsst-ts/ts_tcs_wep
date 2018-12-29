d69b95383a34d1cf0b736292b39a7dc181c19f8a
Te-Wei Tsai <TTsai@lsst.org>
2018-12-29 08:07:29 -0700

    Add the WcsSol class.

---
+++ python/lsst/ts/wep/bsc/WcsSol.py
@@ -0,0 +1,163 @@
+import numpy as np
+
+from lsst.sims.utils import ObservationMetaData
+from lsst.obs.lsstSim import LsstSimMapper
+from lsst.sims.coordUtils.CameraUtils import raDecFromPixelCoords, \
+    pixelCoordsFromRaDec, focalPlaneCoordsFromRaDec
+
+
+class WcsSol(object):
+
+    def __init__(self):
+        
+        self._camera = LsstSimMapper().camera
+        self._obs = ObservationMetaData()
+
+    def setCamera(self, camera):
+        """Set the camera object.
+
+        Parameters
+        ----------
+        camera : Camera
+            A collection of Detectors that also supports coordinate
+            transformation
+        """
+
+        self._camera = camera
+
+    def setObsMetaData(self, ra, dec, rotSkyPos, mjd=59580.0):
+        """Set the observation meta data.
+
+        Parameters
+        ----------
+        ra : float
+            Pointing ra in degree.
+        dec : float
+            Pointing decl in degree.
+        rotSkyPos : float
+            The orientation of the telescope in degrees.
+        mjd : float
+            Camera MJD. (the default is 59580.0.)
+        """
+
+        self._obs = ObservationMetaData(pointingRA=ra, pointingDec=dec,
+                                        rotSkyPos=rotSkyPos, mjd=mjd)
+
+    def raDecFromPixelCoords(self, xPix, yPix, chipName, epoch=2000.0,
+                             includeDistortion=True):
+        """Convert pixel coordinates into RA, Dec.
+
+        WARNING: This method does not account for apparent motion due to
+        parallax. This method is only useful for mapping positions on a
+        theoretical focal plane to positions on the celestial sphere.
+
+        Parameters
+        ----------
+        xPix : float or numpy.ndarray
+            xPix is the x pixel coordinate.
+        yPix : float or numpy.ndarray
+            yPix is the y pixel coordinate.
+        chipName : str or numpy.ndarray
+            chipName is the name of the chip(s) on which the pixel coordinates
+            are defined.  This can be an array (in which case there should be
+            one chip name for each (xPix, yPix) coordinate pair), or a single
+            value (in which case, all of the (xPix, yPix) points will be
+            reckoned on that chip).
+        epoch : float, optional
+            epoch is the mean epoch in years of the celestial coordinate
+            system. (the default is 2000.0.)
+        includeDistortion : bool, optional
+            If True (default), then this method will expect the true pixel
+            coordinates with optical distortion included.  If False, this
+            method will expect TAN_PIXEL coordinates, which are the pixel
+            coordinates with estimated optical distortion removed.  See the
+            documentation in afw.cameraGeom for more details. (the default is
+            True.)
+
+        Returns
+        -------
+        numpy.ndarray
+            A 2-D numpy array in which the first row is the RA coordinate and
+            the second row is the Dec coordinate (both in degrees; in the
+            International Celestial Reference System)
+        """
+
+        if isinstance(chipName, np.ndarray):
+            chipNameList = chipName.tolist()
+        else:
+            chipNameList = chipName
+
+        return raDecFromPixelCoords(xPix, yPix, chipNameList,
+                                    camera=self._camera,
+                                    obs_metadata=self._obs, epoch=epoch,
+                                    includeDistortion=includeDistortion)
+
+    def pixelCoordsFromRaDec(self, ra, dec, chipName=None, epoch=2000.0,
+                             includeDistortion=True):
+        """Get the pixel positions (or nan if not on a chip) for objects based
+        on their RA, and Dec (in degrees).
+
+        Parameters
+        ----------
+        ra : float or numpy.ndarray
+            ra is in degrees in the International Celestial Reference System.
+        dec : float or numpy.ndarray
+            dec is in degrees in the International Celestial Reference System.
+        chipName : numpy.ndarray, str, None
+            chipName designates the names of the chips on which the pixel
+            coordinates will be reckoned. If an array, there must be as many
+            chipNames as there are (RA, Dec) pairs. If a single value, all of
+            the pixel coordinates will be reckoned on the same chip. If None,
+            this method will calculate which chip each(RA, Dec) pair actually
+            falls on, and return pixel coordinates for each (RA, Dec) pair on
+            the appropriate chip. (the default is None.)
+        epoch : float, optional
+            epoch is the mean epoch in years of the celestial coordinate
+            system. (the default is 2000.0.)
+        includeDistortion : bool, optional
+            If True (default), then this method will expect the true pixel
+            coordinates with optical distortion included.  If False, this
+            method will expect TAN_PIXEL coordinates, which are the pixel
+            coordinates with estimated optical distortion removed.  See the
+            documentation in afw.cameraGeom for more details. (the default is
+            True.)
+
+        Returns
+        -------
+        numpy.ndarray
+            A 2-D numpy array in which the first row is the x pixel coordinate
+            and the second row is the y pixel coordinate
+        """
+
+        return pixelCoordsFromRaDec(ra, dec, obs_metadata=self._obs,
+                                    chipName=chipName, camera=self._camera,
+                                    epoch=epoch,
+                                    includeDistortion=includeDistortion)
+
+    def focalPlaneCoordsFromRaDec(self, ra, dec, epoch=2000.0):
+        """Get the focal plane coordinates for all objects in the catalog.
+
+        Parameters
+        ----------
+        ra : float or numpy.ndarray
+            ra is in degrees in the International Celestial Reference System.
+        dec : float or numpy.ndarray
+            dec is in degrees in the International Celestial Reference System.
+        epoch : float, optional
+            epoch is the mean epoch in years of the celestial coordinate
+            system. (the default is 2000.0.)
+
+        Returns
+        -------
+        numpy.ndarray
+            A 2-D numpy array in which the first row is the x focal plane
+            coordinate and the second row is the y focal plane coordinate
+            (both in millimeters)
+        """
+
+        return focalPlaneCoordsFromRaDec(ra, dec, obs_metadata=self._obs,
+                                         epoch=epoch, camera=self._camera)
+
+
+if __name__ == "__main__":
+    pass
---
+++ tests/bsc/testWcsSol.py
@@ -0,0 +1,106 @@
+import numpy as np
+import unittest
+
+from lsst.ts.wep.bsc.WcsSol import WcsSol
+
+
+class TestWcsSol(unittest.TestCase):
+    """Test the ComCam class."""
+
+    def setUp(self):
+
+        self.wcs = WcsSol()
+
+        self.ra = 1.0
+        self.dec = 2.0
+        rotSkyPos = 0.0
+        self.wcs.setObsMetaData(self.ra, self.dec, rotSkyPos)
+
+    def testSetCamera(self):
+
+        camera = "FaultCamera"
+        self.wcs.setCamera(camera)
+
+        self.assertEqual(self.wcs._camera, camera)
+
+    def testSetObservationMetaData(self):
+
+        ra = 10.0
+        dec = 20.0
+        rotSkyPos = 30.0
+        self.wcs.setObsMetaData(ra, dec, rotSkyPos)
+
+        self.assertAlmostEqual(self.wcs._obs.pointingRA, ra)
+        self.assertAlmostEqual(self.wcs._obs.pointingDec, dec)
+        self.assertAlmostEqual(self.wcs._obs.rotSkyPos, rotSkyPos)
+
+    def testRaDecFromPixelCoordsForSingleChip(self):
+
+        xPix = 2032
+        yPix = 2000
+        chipName = "R:2,2 S:1,1"
+        raByWcs, decByWcs = self.wcs.raDecFromPixelCoords(xPix, yPix, chipName)
+
+        self.assertAlmostEqual(raByWcs, self.ra, places=2)
+        self.assertAlmostEqual(decByWcs, self.dec, places=2)
+
+    def testRaDecFromPixelCoordsForChipArray(self):
+
+        xPix = np.array([2032, 2032])
+        yPix = np.array([2000, 2000])
+        chipName = np.array(["R:2,2 S:1,1", "R:2,2 S:1,1"])
+        raByWcs, decByWcs = self.wcs.raDecFromPixelCoords(xPix, yPix, chipName)
+
+        self.assertEqual(len(raByWcs), 2)
+        self.assertEqual(len(decByWcs), 2)
+
+        self.assertAlmostEqual(raByWcs[0], self.ra, places=2)
+        self.assertAlmostEqual(raByWcs[1], self.ra, places=2)
+
+    def testPixelCoordsFromRaDecWithoutChipName(self):
+
+        xPix, yPix = self.wcs.pixelCoordsFromRaDec(self.ra, self.dec)
+
+        self.assertAlmostEqual(xPix, 2032, places=-1)
+        self.assertAlmostEqual(yPix, 1994, places=-1)
+
+    def testPixelCoordsFromRaDecWithChipName(self):
+
+        chipName = "R:2,2 S:1,1"
+        xPix, yPix = self.wcs.pixelCoordsFromRaDec(self.ra, self.dec,
+                                                   chipName=chipName)
+
+        self.assertAlmostEqual(xPix, 2032, places=-1)
+        self.assertAlmostEqual(yPix, 1994, places=-1)
+
+    def testFocalPlaneCoordsFromRaDecWithZeroRot(self):
+
+        self.wcs.setObsMetaData(0, 0, 0)
+        xInMm, yInMm = self.wcs.focalPlaneCoordsFromRaDec(0, 0)
+
+        self.assertEqual(xInMm, 0.0)
+        self.assertEqual(yInMm, 0.0)
+
+        # 0.2 arcsec = 10 um => 1 um = 0.02 arcsec => 1 mm = 20 arcsec
+        # 1 arcsec = 1/3600 degree
+        xInMm, yInMm = self.wcs.focalPlaneCoordsFromRaDec(20.0/3600, 0)
+
+        self.assertAlmostEqual(xInMm, 1.0, places=3)
+        self.assertAlmostEqual(yInMm, 0.0, places=3)
+
+    def testFocalPlaneCoordsFromRaDecWithNonZeroRot(self):
+
+        self.wcs.setObsMetaData(0, 0, 45)
+
+        # 0.2 arcsec = 10 um => 1 um = 0.02 arcsec => 1 mm = 20 arcsec
+        # 1 arcsec = 1/3600 degree
+        xInMm, yInMm = self.wcs.focalPlaneCoordsFromRaDec(20.0/3600, 0)
+
+        self.assertAlmostEqual(xInMm, 1/np.sqrt(2), places=3)
+        self.assertAlmostEqual(yInMm, -1/np.sqrt(2), places=3)
+
+
+if __name__ == "__main__":
+
+    # Do the unit test
+    unittest.main()
